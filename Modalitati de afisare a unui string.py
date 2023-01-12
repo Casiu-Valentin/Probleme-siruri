279b4a50aaca8fbe875c808d8a2b22a3f2079f7b
George Valentin Casiu <casrom@gmail.com>
2023-01-12 16:21:15 +0200

    Update Modalitati de afisare a unui string.py

--- Modalitati de afisare a unui string.py
+++ Modalitati de afisare a unui string.py
@@ -19,35 +19,45 @@ Introduceti textul/Enter the text:Ana are mere.
 
 s=input("Introduceti textul/Enter the text:")
 
+#varianta/version 1
 print('1. ',end='')
 print(s)
 
+#varianta/version 2
 print('2. ',end='')
 print(s[:])
 
+#varianta/version 3
 print('3. ',end='')
 print(s[::])
 
+#varianta/version 4
 print('4. ',end='')
 print(s[::1])
 
+#varianta/version 5
 print('5. ',end='')
 print(s[0:])
 
+#varianta/version 6
 print('6. ',end='')
 print(s[:len(s)])
 
+#varianta/version 7
 print('7. ',end='')
 print(s[0:len(s)])
 
+#varianta/version 8
 print('8. ',end='')
 print(s[0:len(s):1])
 
+#varianta/version 9
 print('9. ',end='')
 for i in range(len(s)):
     print(s[i], end='')
 print()
 
+#varianta/version 10
 print('10. ',end='')
 i=0
 while i<len(s):
